302f6ca8b71da0acd4287f2d8053110a22f137a9
Joseph <josephylee72@gmail.com>
2023-10-25 14:24:47 -0700

    completed oxford comma

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,11 @@
-def oxford_comma(items):
-    return None
+def oxford_comma(items : list):
+    if len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        return f"{items[0]} and {items[1]}"
+    else:
+        copy = items
+        popped_item = copy.pop()
+        return f"{', '.join(copy)}, and {popped_item}"
+
+oxford_comma(['poo', 'pee', 'penis'])
